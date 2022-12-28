0ddb238c20424de8c974e5d3ce1ee246374873b3
ArnurSakenov <arnur.s@mail.ru>
2022-12-28 12:00:22 +0600

    Initial Commit

---
+++ fitApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fitApp
+//
+//  Created by Arnur Sakenov on 28.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
